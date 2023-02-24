5debf0c54e35d367bcaa710bbab4dd6a5641f1b7
Nicolas <devnicolas27@gmail.com>
2023-02-24 14:06:38 -0500

    Create generic views inside of function views

--- thebestawards/polls/urls.py
+++ thebestawards/polls/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("<int:question_id>", views.detail, name='detail'),
-    path("results/<int:question_id>", views.results, name='results'),
+    path("", views.IndexView.as_view(), name='index'),
+    path("<int:pk>", views.DetailView.as_view(), name='detail'),
+    path("results/<int:pk>", views.ResultView.as_view(), name='results'),
     path("vote/<int:question_id>", views.vote, name='vote'),
 ]
