1d4feaec2e0fcb1a6221845dfb2c06c94e4d3ac3
Nicolas <devnicolas27@gmail.com>
2023-02-27 10:32:22 -0500

    More test add and view and models modified

--- thebestawards/polls/tests.py
+++ thebestawards/polls/tests.py
@@ -2,14 +2,65 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-
+from django.urls.base import reverse
 from .models import Question
 
 #Models
 #Views
 class QuestionModelTests(TestCase):
+    def setUp(self):
+        self.question = Question(question_text="What was the best afition?")
+        
     def test_was_publisehed_recently_future_questions(self):
         '''was_published_recently return false for Q whose pub_date is in the future'''
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="What was the best afition?", pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
+
+    def test_was_publisehed_recently_present_questions(self):
+        '''was_published_recently return false for Q whose pub_date is in the present'''
+        time = timezone.now()
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), True)
+
+    def test_was_publisehed_recently_past_questions(self):
+        '''was_published_recently return false for Q whose pub_date is in the past'''
+        time = timezone.now() - datetime.timedelta(days=30)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
+
+class QuestionIndexViewTests(TestCase):
+    def setUp(self):
+        self.question = Question(question_text="What was the best afition?")
+
+    def test_no_questions(self):
+        '''If no question exists, an appropiate message is displayed'''
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_display_index(self):
+        '''If a Q is created with a future pub_date, should not display on index'''
+        response = self.client.get(reverse("polls:index"))
+        time = timezone.now() + datetime.timedelta(days=30)
+        self.question.pub_date = time
+        self.assertNotIn(self.question, response.context["latest_question_list"])
+
+    def two_questions_future(self):
+        '''If two Q are created with a future pub_date, should not display on index'''
+        response = self.client.get(reverse("polls:index"))
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_Q1 = self.question.pub_date = time
+        future_Q2 = self.question.pub_date = time
+        self.assertNotIn(future_Q1, future_Q2, response.context["latest_question_list"])
+
+    
+        
+        
+
+
+
+
+    
+
